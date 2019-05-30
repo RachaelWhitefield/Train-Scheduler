066f7d0608269065bbde151e6346752a2cac8764
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-05-29 22:08:10 -0600

    working on train timing

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,14 +12,14 @@ var firebaseConfig = {
 
 var database = firebase.database();
 
-// 2. Button for adding TRains
+// 2. Button for adding Trains
 $("#add-train-btn").on("click", function(event) {
   event.preventDefault();
 
   // Grabs user input
   var trainName = $("#train-name-input").val().trim();
   var trainDest = $("#destination-input").val().trim();
-  var trainTime = moment($("#first-input").val().trim(), "MM/DD/YYYY").format("X");
+  var trainTime = moment($("#first-input").val().trim(), "HH:mm").format("X");
   var trainFreq = $("#frequency-input").val().trim();
 
   // Creates local "temporary" object for holding train data
@@ -48,9 +48,9 @@ $("#add-train-btn").on("click", function(event) {
   $("#frequency-input").val("");
 });
 
-// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+// 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot) {
-  console.log(childSnapshot.val());
+//   console.log(childSnapshot.val());
 
   // Store everything into a variable.
   var trainName = childSnapshot.val().name;
@@ -61,20 +61,9 @@ database.ref().on("child_added", function(childSnapshot) {
   // Employee Info
   console.log(trainName);
   console.log(trainDest);
-  console.log(trainTime);
+//   console.log(trainTime);
   console.log(trainFreq);
 
-  // Prettify the employee start
-//   var trainStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
-
-  // Calculate the months worked using hardcore math
-  // To calculate the months worked
-//   var empMonths = moment().diff(moment(empStart, "X"), "months");
-//   console.log(empMonths);
-
-  // Calculate the total billed rate
-//   var empBilled = empMonths * empRate;
-//   console.log(empBilled);
 
   // Create the new row
   var newRow = $("<tr>").append(
@@ -89,10 +78,6 @@ database.ref().on("child_added", function(childSnapshot) {
   $("#train-table > tbody").append(newRow);
 });
 
-// Example Time Math
-// -----------------------------------------------------------------------------
-// Assume Employee start date of January 1, 2015
-// Assume current date is March 1, 2016
+var currentTime = moment();
+console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
 
-// We know that this is 15 months.
-// Now we will create code in moment.js to confirm that any attempt we use meets this test case
